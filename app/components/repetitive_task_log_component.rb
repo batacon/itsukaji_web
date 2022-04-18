6468d7040213882091e561d902826bba3c0cdd8e
kawarabata <kwrbt0404@gmail.com>
2022-04-18 22:32:25 +0900

    やったログで今日なら今日と表示

--- app/components/repetitive_task_log_component.rb
+++ app/components/repetitive_task_log_component.rb
@@ -4,4 +4,13 @@ class RepetitiveTaskLogComponent < ViewComponent::Base
   def initialize(repetitive_task_log:)
     @repetitive_task_log = repetitive_task_log
   end
+
+  def date
+    @repetitive_task_log.date
+  end
+
+  def how_many_days_ago
+    days_number = @repetitive_task_log.how_many_days_ago
+    days_number.zero? ? '今日' : "#{days_number}日前"
+  end
 end
