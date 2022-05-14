f91f4ee05100ed85285984d09ab80f11fe57a60d
kawarabata <kwrbt0404@gmail.com>
2022-05-14 23:03:23 +0900

    HeaderMenuComponentはcurrent_userのことを知らなくていい

--- app/components/header_menu_component.rb
+++ app/components/header_menu_component.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 class HeaderMenuComponent < ViewComponent::Base
-  def initialize(current_user:)
-    @current_user = current_user
-  end
-
-  private
-
-  def render?
-    !!@current_user
-  end
-
-  def current_user
-    @current_user
-  end
 end
--- test/components/header_menu_component_test.rb
+++ test/components/header_menu_component_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class HeaderMenuComponentTest < ViewComponent::TestCase
   it '正しく表示される' do
-    render_inline(HeaderMenuComponent.new(current_user: users(:member1_of_group1)))
+    render_inline(HeaderMenuComponent.new())
     dots = page.find('img')
     expect(dots[:src]).must_include 'icon_three_dots'
 
@@ -13,11 +13,5 @@ class HeaderMenuComponentTest < ViewComponent::TestCase
     expect(a_tags[0].text).must_equal '招待・共有設定'
     expect(a_tags[1][:href]).must_equal '/log_out'
     expect(a_tags[1].text).must_equal 'ログアウト'
-  end
-
-  it 'ログインしていない場合は表示されない' do
-    render_inline(HeaderMenuComponent.new(current_user: nil))
-    assert_no_selector 'button'
-    assert_no_selector 'a'
   end
 end
